4a06a45e6808386046d82e4df32cc74a6c5a7dbb
Sammy filly <136061549+X-oss-byte@users.noreply.github.com>
2023-09-26 17:46:15 +0100

    Design docs (#10)
    * update: homepage
    
    * yarn upgardes
    
    * update: features section homepage
    
    * fix: features icons
    
    * fix: features section in dark mode
    
    * fix: title line height
    
    * refactor: section components
    
    ---------
    
    Co-authored-by: Alexandre Chopin <alex@nuxtjs.com>
    Co-authored-by: Sébastien Chopin <seb@nuxtjs.com>

---
+++ docs/components/content/templates/nSection.vue
@@ -0,0 +1,7 @@
+<template>
+  <section class="text-center bg-gray-50 border-y border-gray-100 dark:border-gray-700 dark:bg-gray-800 flex min-h-screen px-4 py-20  sm:px-6 md:px-8">
+    <Container class="flex flex-col justify-around">
+      <slot />
+    </Container>
+  </section>
+</template>
